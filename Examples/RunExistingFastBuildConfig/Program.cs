a141defb590f34d24fac954b925b5aee07a5f1ad
samoatesgames <sam@samoatesgames.com>
2020-02-02 19:22:21 +0000

    Make TexConv and FastBuild work
    I forked TexConv and made it so specifying an absolute path to the -o argument it removed the path and just used the folder.
    This happens to make the file name patterns match for fastbuild and texconv, allowing TexConv to work with FastBuild.

--- Examples/RunExistingFastBuildConfig/Program.cs
+++ Examples/RunExistingFastBuildConfig/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using FastBuild.Net;
 
@@ -8,12 +9,23 @@ namespace RunExistingFastBuildConfig
     {
         public static async Task Main(string[] args)
         {
+            if (Directory.Exists("FastBuild/output/"))
+            {
+                foreach (var file in Directory.EnumerateFiles("FastBuild/output/"))
+                {
+                    File.Delete(file);
+                }
+            }
+
+            SetupTestData("FastBuild/input/logo.png", 1000);
+
             var startInfo = new FastBuildStartInfo
             {
                 FastBuildExecutableLocation = "../../../../../External/FastBuild/0.99/FBuild.exe",
                 FastBuildConfigLocation = "FastBuild/FBuild.bff",
                 ShowCommands = true,
-                Verbose = true
+                Verbose = true,
+                GenerateReport = true
             };
 
             try
@@ -36,6 +48,34 @@ namespace RunExistingFastBuildConfig
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
+        }
+
+        private static void SetupTestData(string sourceImageLocation, int imageCount)
+        {
+            var sourceImagePath = Path.GetFullPath(sourceImageLocation);
+            if (!File.Exists(sourceImagePath))
+            {
+                Console.WriteLine($"Failed to find the source input image '{sourceImagePath}'");
+                Console.ReadLine();
+                return;
+            }
+
+            var sourceDirectory = Path.GetDirectoryName(sourceImagePath);
+            var sourceNoExtension = Path.GetFileNameWithoutExtension(sourceImagePath);
+            var sourceExtension = Path.GetExtension(sourceImagePath);
+
+            if (sourceDirectory == null)
+            {
+                Console.WriteLine($"Failed to get the source directory of the image path '{sourceImagePath}'");
+                Console.ReadLine();
+                return;
+            }
+
+            for (var i = 0; i < imageCount; ++i)
+            {
+                var targetPath = Path.Combine(sourceDirectory, $"{sourceNoExtension}-{i}.{sourceExtension}");
+                File.Copy(sourceImagePath, targetPath, true);
+            }
         }
     }
 }
